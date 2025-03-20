4e32b1333e1eda7939cf69667c16b3983185fee1
Val Huber <valjhuber@gmail.com>
2025-03-19 21:04:30 -0700

    remove obsolete comments

--- run_web_genai.sh
+++ run_web_genai.sh
@@ -1,7 +1,4 @@
-# Run WebGenAI locally from docker container
-
-# Before running, update ./webgenai/webg_config/web_genai.txt
-# See: https://apilogicserver.github.io/Docs/WebGenAI-CLI/#configuration
+# Run WebGenAI from docker container
 
 # cd genai-logic-main
 # sh run_web_genai.sh
